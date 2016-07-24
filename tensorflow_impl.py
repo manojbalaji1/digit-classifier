53f9886f3dd2b9b8c1a770db233276035b441077
Karan Desai <karandesai_96@live.com>
2016-07-24 15:12:28 +0530

    Add training and testing steps.

--- tensorflow_impl.py
+++ tensorflow_impl.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -65,3 +66,28 @@ W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
 
 y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv),
+                                              reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, dtype=tf.float32))
+
+session.run(tf.initialize_all_variables())
+
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+
+    if i % 100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={
+            x: batch[0], y_: batch[1], keep_prob: 1.0
+        })
+        print("Step %d, training_accuracy %g" % (i, train_accuracy))
+    train_step.run(feed_dict={
+        x: batch[0], y_: batch[1], keep_prob: 0.5
+    })
+
+print("Test accuracy %g" % accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0
+}))
